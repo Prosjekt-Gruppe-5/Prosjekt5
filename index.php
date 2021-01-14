e03ca88e457cffc5fb82325337f40abb61db4091
Ingrid <ingrid.e.dahl@hiof.no>
2021-01-14 20:48:26 +0100

    Frontend elements for sending message for chosen subject

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+//Mock "database" for testing av funksjonalitet, som må byttes ut med ekte DB når denne er oppe
+    $mockDB = array(
+        '1' => array(
+            'name' => 'Testemne 1',
+            'lecturer' => 'Per Pettersen',
+        ),
+
+        '2' => array(
+            'name' => 'Testemne 2',
+            'lecturer' => 'Gunn Gundersen',
+        ),
+
+        '3' => array(
+            'name' => 'Testemne 3',
+            'lecturer' => 'Ole Olsen',
+        )
+        );
+?>
 
 <!DOCTYPE html>
 <html>
@@ -22,19 +41,46 @@
 
 
         <div id="featured-content">
-            <h1>....</h1>
-            <p>....</p>
+            <form action='index.php' method='get'>
+                <label for='subject'>Emne</label>
+
+                <select name='subject' id='subject'>
+                    <option value='0' disabled='true' selected='true'>-- Velg emne --</option>
+                    <?php 
+                        foreach($mockDB as $key => $value) {
+                            echo "
+                            <option value=" . $key . ">" . $mockDB[$key]['name'] . "</option>
+                            ";
+                        }
+                    ?>
+                </select>
+
+                <input type='submit' value='Velg'>
+            </form>
+
+            <?php 
+                if(ISSET($_GET['subject']) && $_GET['subject'] != '0') {
+                    echo "<p>Emne: " . $mockDB[$_GET['subject']]['name'] . "</p>
+                    <p>Foreleser: " . $mockDB[$_GET['subject']]['lecturer'] . "</p>
+                    <form action='index.php?subject=" . $_GET['subject'] . "' method='post' id='messageForm'>
+                        <input type='hidden' name='messageSubject' id='messageSubject' value=" . $_GET['subject'] . ">
+                        <label for='message'>Melding til foreleser</label>
+                        <textarea id='message' name='message'></textarea>
+                        <input type='submit' value='Send'>
+                    </form>
+                    ";
+                }
+            ?>
+
+            <?php
+                if(ISSET($_POST['message'])) {
+                    echo "<p>Melding sendt for emne: " . $mockDB[$_POST['messageSubject']]['name'] . "</p>
+                    <p>Din melding: " . $_POST['message'] . "</p>";
+                }
+            ?>
+            
         </div>
-
-       
-
-        <footer>
-
-            <p>...</p>
-
-        </footer>
 
     </div>
 </body>
 </html>
-
