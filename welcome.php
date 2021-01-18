11b1d1dfe41e99278a3fed77f9d9287d041f6d38
Henriette Mathisen <66080359+Northway00@users.noreply.github.com>
2021-01-18 16:08:04 +0100

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,31 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Velkommen</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1><?php echo htmlspecialchars($_SESSION["username"]); ?></b>Velkommen</h1>
+    </div>
+    <p>
+        <a href="login.php" class="btn btn-primary">Login her</a>
+        <a href="registrer.php" class="btn btn-primary">Registrer deg</a>
+    </p>
+</body>
+</html>
