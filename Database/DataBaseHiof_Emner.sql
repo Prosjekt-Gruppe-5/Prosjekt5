707d1bdeb863e2033721440b5ab026f7de07af41
Makkabro <69955450+Makkabro@users.noreply.github.com>
2021-01-25 16:21:01 +0100

    Add files via upload

---
+++ Database/DataBaseHiof_Emner.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: DataBaseHiof
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Emner`
+--
+
+DROP TABLE IF EXISTS `Emner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Emner` (
+  `idEmne` int NOT NULL,
+  `Emnekode` varchar(45) NOT NULL,
+  `EmneNavn` varchar(45) NOT NULL,
+  `idForeleser` int NOT NULL,
+  PRIMARY KEY (`idEmne`),
+  UNIQUE KEY `idFag_UNIQUE` (`idEmne`),
+  UNIQUE KEY `Emnekode_UNIQUE` (`Emnekode`),
+  KEY `emner_ibfk_1` (`idForeleser`),
+  CONSTRAINT `emner_ibfk_1` FOREIGN KEY (`idForeleser`) REFERENCES `Foreleser` (`idForeleser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Emner`
+--
+
+LOCK TABLES `Emner` WRITE;
+/*!40000 ALTER TABLE `Emner` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Emner` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-25 16:17:53
