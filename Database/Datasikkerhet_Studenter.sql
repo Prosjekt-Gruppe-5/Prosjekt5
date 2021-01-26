c3573b5276ba1ab9dfc57604a7c566717f3880ac
Makkabro <69955450+Makkabro@users.noreply.github.com>
2021-01-26 13:12:08 +0100

    Add files via upload

---
+++ Database/Datasikkerhet_Studenter.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: Datasikkerhet
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Studenter`
+--
+
+DROP TABLE IF EXISTS `Studenter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Studenter` (
+  `Student_id` int NOT NULL AUTO_INCREMENT,
+  `Fornavn` varchar(45) NOT NULL,
+  `Etternavn` varchar(45) NOT NULL,
+  `Epost` varchar(45) NOT NULL,
+  `Kull` varchar(45) NOT NULL,
+  `Passord` varchar(45) NOT NULL,
+  `Studieretning_id` int NOT NULL,
+  PRIMARY KEY (`Student_id`),
+  UNIQUE KEY `Epost_UNIQUE` (`Epost`),
+  KEY `Studieretning_id` (`Studieretning_id`),
+  CONSTRAINT `studenter_ibfk_1` FOREIGN KEY (`Studieretning_id`) REFERENCES `Studieretning` (`Studieretning_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Studenter`
+--
+
+LOCK TABLES `Studenter` WRITE;
+/*!40000 ALTER TABLE `Studenter` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Studenter` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-26 13:11:09
