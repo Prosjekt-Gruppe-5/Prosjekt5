f5f793e8ae1378cf31f559760bc190cf68933285
Harani1104 <69955269+Harani1104@users.noreply.github.com>
2021-02-01 15:30:33 +0100

    Add files via upload

---
+++ velkommen.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Velkommen til HIOF</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+
+</head>
+
+<body>
+    <div id="wrapper">
+
+        <div id="nav">
+            <a href="velkommen.php">Hjem</a>
+            <a href="index.php">Emne</a>
+            <a href="om.php">Om oss</a>
+            <span class="regis"><a href = "registrer.php">Registrer</a></span>
+            <span class="regis"><a href = "login.php">Login</a></span>
+        </div>
+
+
+        <div id="featured-content2">
+            <p>
+                <h1>Velkommen til HIOF</h1>
+
+                
+
+            </p>
+           
+            
+        </div>
+
+    </div>
+</body>
+</html>
