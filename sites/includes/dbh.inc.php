9ad56757e877de5f308086f5520c629da87f9e80
Herman Kristiansen <36899780+shadowrake@users.noreply.github.com>
2021-02-03 15:50:30 +0100

    Add files via upload

--- sites/includes/dbh.inc.php
+++ sites/includes/dbh.inc.php
@@ -6,3 +6,9 @@ $dbPassword = "?8QGb@_M]";
 $dbName = "datasikkerhet";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+ }
+ echo "Connected successfully";
+ ?>
