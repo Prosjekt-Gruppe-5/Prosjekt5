86e2623ac2a84faf89fefec36ea9b4ac3e9ed9de
Herman Kristiansen <gghh@live.no>
2021-02-04 11:54:56 +0100

    jada dette tar tid

--- sites/includes/dbh.inc.php
+++ sites/includes/dbh.inc.php
@@ -1,19 +1,3 @@
-<<<<<<< Updated upstream
-<?php
-
-$dbServername = "158.39.188.205";
-$dbUsername = "datasikkerhet";
-$dbPassword = "?8QGb@_M]";
-$dbName = "datasikkerhet";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
- }
- echo "Connected successfully";
- ?>
-=======
 <?php
 
 $dbServername = "158.39.188.205";
--- sites/includes/test.inc.php
+++ sites/includes/test.inc.php
@@ -4,10 +4,3 @@ include 'db_connection.php';
  
 echo "Connected Successfully";
 mysqli_close($conn);
-=======
-<?php
-include 'dbh.inc.php';
- 
-echo "Connected Successfully";
-mysqli_close($conn);
->>>>>>> Stashed changes
