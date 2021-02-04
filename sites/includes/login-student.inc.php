0849da0a857f6f30531f9444f1a2bd9fa01b7ee6
Herman Kristiansen <gghh@live.no>
2021-02-04 16:24:59 +0100

    pathing

--- sites/includes/login-student.inc.php
+++ sites/includes/login-student.inc.php
@@ -60,7 +60,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["email"] = $email;                            
                             
                             // Redirect user to welcome page
-                            header("location: ../welcome.php");
+                            header("location: ../../index.php?logginn=success");
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
