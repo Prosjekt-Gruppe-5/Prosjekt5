db693b0aab615ef0f77ad7599d9038876b645e74
Herman Kristiansen <gghh@live.no>
2021-02-05 21:32:15 +0100

    gjest

--- sites/message_gjest.php
+++ sites/message_gjest.php
@@ -10,11 +10,17 @@ include_once "includes/dbh.inc.php";
 
 if(isset($_POST["submit"])) 
             {
-        $sql = sprintf("UPDATE Meldinger SET Svar = '". $_POST['message1'] ."' WHERE Melding_id = '". $_POST['test'] ."'");              
+        $sql = sprintf("SELECT Kommentartekst FROM Kommentar WHERE Melding_id = '". $_POST['test'] ."'");              
             $conn->query($sql);
             //var_dump($conn);
             }
-            
+            if(isset($_POST["submit1"])) 
+            {
+            $sql = sprintf("SELECT * FROM rapport");      
+            $conn->real_escape_string($_POST["hei"]);       
+            $conn->query($sql);
+            //var_dump($conn);
+            }
 $foreleser = "SELECT * FROM Foreleser";
 $foreleser_conn = $conn->query($foreleser);
 
@@ -24,7 +30,7 @@ $emner_conn = $conn->query($emner);
 $meldinger = "SELECT * FROM Meldinger";
 $meldinger_conn = $conn->query($meldinger);
 
-$meldinger_1 = "SELECT * FROM Meldinger";
+$meldinger_1 = "SELECT * FROM Kommentar WHERE Kommentar.Melding_id = Meldinger.Melding_id";
 $meldinger_1_conn = $conn->query($meldinger_1);
 ?>
 <!DOCTYPE html>
@@ -38,12 +44,19 @@ $meldinger_1_conn = $conn->query($meldinger_1);
 </head>
 <body>
 <div id="featured-content">
+            <form action="" method="get">
             <input type="number" name="pin">
-            <?php if(isset('Pin')) ?>
+            <input type="submit" value="sumbit" name="submit">
+            </form>
+            <?php if(isset($_GET['pin']) != NULL) {?>
            <p><strong>Alle meldinger fra studenter: </strong></p>
             <?php while($rad1 = mysqli_fetch_array($meldinger_conn)) {?>
             <p><?php echo $rad1["Melding_id"] ?></p></span>
             <p><?php echo $rad1["Meldingstekst"] ?></p></span>
+            <form method="get">
+                <input type="text" readonly value="Upassende/Spam" name="hei">
+                <input type="submit" value="Rapport" name="sumbit1">
+            </form>
             <?php } ?>
             <form method='POST'>
                 <label for='message'><strong>Svar til student:</strong></label>
@@ -55,6 +68,6 @@ $meldinger_1_conn = $conn->query($meldinger_1);
                 </select>
                 <input type='submit' value='submit' name='submit'>
             </form>
-    
+            <?php } ?>    
 </body>
 </html>
